243548f9b0d9d4a69821d3da1dac0c1480b090b3
David Benson <dmbenson1978@gmail.com>
2026-06-05 00:15:34 +0100

    [CODE/TEST] scheduling-api - POST response - PASS

--- app/api/scheduling/route.ts
+++ app/api/scheduling/route.ts
@@ -1,4 +1,11 @@
 import { NextRequest } from "next/server";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const POST = async (request: NextRequest) => { }
+export const POST = async (request: NextRequest) => {
+  return new Response(JSON.stringify({
+      unbuiltAPI: true,
+    }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+}
