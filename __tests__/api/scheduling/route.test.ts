708aaa98ea0d52ae9b4f3a77a89cc623f4a3dc90
David Benson <dmbenson1978@gmail.com>
2026-06-05 12:26:30 +0100

    [TEST] Read a single reservation in hydrated form - API method exists - FAIL

--- __tests__/api/scheduling/route.test.ts
+++ __tests__/api/scheduling/route.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest'
 
 import { NextRequest } from 'next/server'
 
-import { POST } from '../../../app/api/scheduling/route'
+import { GET, POST } from '../../../app/api/scheduling/route'
 import { testShedulingAPIURL } from '../../../mocks/msw.mock'
 
 describe('POST - scheduling-api', () => {
@@ -18,6 +18,20 @@ describe('POST - scheduling-api', () => {
     expect(response.status).toEqual(200)
     expect(contentType).toEqual('application/json')
     expect(json.unbuiltAPI).toEqual(true)
+  })
+
+  it('should read a single reservation in hydrated form - GET /?reservation={id}', async () => {
+    const reservationId = 1
+    const request = new NextRequest(new Request(`${testShedulingAPIURL}?reservation=${reservationId}`, {
+      method: 'GET',
+    }))
+    const response = await GET(request)
+    const contentType = response.headers.get('Content-Type')
+    const json = await response.json();
+
+    expect(response.status).toEqual(200)
+    expect(contentType).toEqual('application/json')
+    expect(json.resourceId).toEqual(1)
   })
 
 })
