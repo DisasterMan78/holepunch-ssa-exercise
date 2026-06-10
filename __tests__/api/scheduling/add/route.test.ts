5533075430a1350056a283e2fa612e8b1e34d33e
David Benson <dmbenson1978@gmail.com>
2026-06-10 04:36:37 +0100

    [TEST] Add new Reservation record - FAIL

---
+++ __tests__/api/scheduling/add/route.test.ts
@@ -0,0 +1,62 @@
+import { describe, expect, it } from 'vitest'
+
+import { NextRequest } from 'next/server'
+
+import { POST } from '../../../../app/api/scheduling/add/route'
+import { testSchedulingAddAPIURL } from '../../../../mocks/msw.mock'
+
+describe('POST - scheduling-api', () => {
+
+  it('should return 400 if the JSON is not present', async () => {
+    const request = new NextRequest(new Request(testSchedulingAddAPIURL, {
+      method: 'POST',
+    }))
+    const response = await POST(request)
+    const contentType = response.headers.get('Content-Type')
+    const json = await response.json();
+
+    expect(response.status).toEqual(400)
+    expect(contentType).toEqual('application/json')
+    expect(json.errorMessage).toEqual('Invalid request body - request body not present or empty')
+  })
+
+
+  it('should return 400 if the payload is not present', async () => {
+    const request = new NextRequest(new Request(testSchedulingAddAPIURL, {
+      method: 'POST',
+      body: JSON.stringify({})
+    }))
+    const response = await POST(request)
+    const contentType = response.headers.get('Content-Type')
+    const json = await response.json();
+
+    expect(response.status).toEqual(400)
+    expect(contentType).toEqual('application/json')
+    expect(json.errorMessage).toEqual('Invalid request body - payload contains no data')
+  })
+
+
+  it('should add a reservation - POST /scheduling/add/', async () => {
+    const request = new NextRequest(new Request(testSchedulingAddAPIURL, {
+      method: 'POST',
+      body: JSON.stringify({
+        timezone: "Europe/London",
+        holder: "dmbenson1978@gmail.com",
+        resourceId: 5,
+        startsAt: "2026-06-30T11:05:00.000Z",
+        endsAt: "2026-06-30T14:03:00.000Z"
+      })
+    }))
+    const response = await POST(request)
+    const contentType = response.headers.get('Content-Type')
+    const json = await response.json();
+
+    expect(response.status).toEqual(201)
+    expect(contentType).toEqual('application/json')
+    expect(json.id).toBeTruthy()
+    expect(json.resourceId).toEqual(5)
+    expect(json.holder).toEqual("dmbenson1978@gmail.com")
+    expect(json.startsAt).toEqual("2026-06-30T11:05:00.000Z")
+    expect(json.endsAt).toEqual("2026-06-30T14:03:00.000Z")
+  })
+})
